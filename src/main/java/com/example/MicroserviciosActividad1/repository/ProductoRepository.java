acc4f464c505aa728a181fd16287cf947bce6698
nbello26 <nicolas.vicente1226@gmail.com>
2026-03-10 18:59:08 -0300

    Modificar repository

--- src/main/java/com/example/MicroserviciosActividad1/repository/ProductoRepository.java
+++ src/main/java/com/example/MicroserviciosActividad1/repository/ProductoRepository.java
@@ -1,8 +1,10 @@
 package com.example.MicroserviciosActividad1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.MicroserviciosActividad1.model.Producto;
+@Repository
 public interface ProductoRepository extends JpaRepository<Producto, Long>{
     
 }
