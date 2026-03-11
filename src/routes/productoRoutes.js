9b7fc0b29979f203747ab350907bb2a63055f46f
Nicolas <Nicolas.vicente1226@gmail.com>
2026-03-10 21:11:01 -0300

    Mejora a documentacion Swagger

--- src/routes/productoRoutes.js
+++ src/routes/productoRoutes.js
@@ -9,6 +9,9 @@ const productoController = require("../controllers/productoController");
  * /productos:
  *   get:
  *     summary: Obtener todos los productos
+ *     responses:
+ *       200:
+ *         description: Lista de productos
  */
 router.get("/", productoController.obtenerProductos);
 
@@ -17,6 +20,18 @@ router.get("/", productoController.obtenerProductos);
  * /productos/{id}:
  *   get:
  *     summary: Obtener producto por ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del producto
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Producto encontrado
+ *       404:
+ *         description: Producto no encontrado
  */
 router.get("/:id", productoController.obtenerProductoPorId);
 
@@ -25,6 +40,24 @@ router.get("/:id", productoController.obtenerProductoPorId);
  * /productos:
  *   post:
  *     summary: Crear producto
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre:
+ *                 type: string
+ *               precio_clp:
+ *                 type: integer
+ *               descripcion:
+ *                 type: string
+ *               stock:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Producto creado
  */
 router.post("/", productoController.crearProducto);
 
@@ -33,6 +66,18 @@ router.post("/", productoController.crearProducto);
  * /productos/{id}:
  *   delete:
  *     summary: Eliminar producto
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID del producto
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Producto eliminado
+ *       404:
+ *         description: Producto no encontrado
  */
 router.delete("/:id", productoController.eliminarProducto);
 
